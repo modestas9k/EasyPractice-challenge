b475cc80e9f7d1a0b2b55cbf40d7f222da7399ce
Modestas Skirius <modestas.skirius@gmail.com>
2022-01-15 20:19:24 +0200

    add simple card component

---
+++ src/components/SimpleCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center shadow-md bg-white rounded-md p-4 ">
+    <h2 class="font-bold text-xl my-2">{{ title }}</h2>
+    <h1 class="font-bold text-3xl mb-4">{{ bigText}}</h1>
+    <p class="text-gray-400">{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SimpleCard",
+  props: {
+    title: String,
+    bigText: String,
+    description: String
+  },
+};
+</script>
