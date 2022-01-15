9c68c12334a462cf30de5a2acb4eb45e339953c9
Modestas Skirius <modestas.skirius@gmail.com>
2022-01-15 20:19:14 +0200

    add paymentCard component

---
+++ src/components/PaymentCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div :class="[status === 'Charged' ? 'border-l-8 border-blue-400' : 'border-l-8']" 
+        class="bg-white rounded-md p-4  shadow-md flex flex-row gap-4">
+        <span class="flex-1 text-sm font-medium">{{ formatDate(date) }}</span>
+        <span class="flex-1 text-sm font-medium text-gray-400">{{ status }}</span>
+        <span class="flex-1 text-sm font-medium text-gray-400 truncate">{{ clientName}}</span>
+        <span class="flex-1 text-sm font-medium text-gray-400">{{ currency }} {{ amount}}</span>
+    </div>
+</template>
+<script>
+import moment from 'moment';
+
+export default {
+    name: 'PaymentCard',
+    props: {
+        date: String,
+        status: String,
+        clientName: String,
+        currency: String,
+        amount: Number
+    },
+    methods: {
+        formatDate(date) {
+            return moment(date).format('DD. MMMM YYYY, HH:mm');
+        }
+    }
+}
+</script>
